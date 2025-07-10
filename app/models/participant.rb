796d4303caeb1e154b6f583ea6d45e94273c79b9
Kelly Cruz <30739866+kellypc@users.noreply.github.com>
2025-07-10 20:30:38 +0000

    Add callback to email to participant

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,14 @@
 class Participant < ApplicationRecord
+  before_validation :downcase_email
+
   has_many :registrations, dependent: :destroy
   has_many :talks, through: :registrations
   validates :name, :email, presence: true
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  private
+
+  def downcase_email
+    email.downcase! if email
+  end
 end
